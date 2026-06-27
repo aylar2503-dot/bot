6ce47b09364a6c38be0e002402c140ef9b7992b2
aylar2503-dot <aylar2503@gmail.com>
2026-06-27 21:59:01 +0300

    Add imports for aiohttp and aiogram libraries

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
+import os
+import logging
 
+from aiohttp import web
+from aiogram import Bot, Dispatcher, Router
+from aiogram.filters import CommandStart
+from aiogram.types import Message, Update
